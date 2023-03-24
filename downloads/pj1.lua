5a8cb5edc13a97add211dedfdd2eecbb65e52acc
41023146 <41023146@gm.nfu.edu.tw>
2023-03-24 23:41:44 +0800

    change pj1.lua

--- downloads/pj1.lua
+++ downloads/pj1.lua
@@ -3,15 +3,17 @@ function sysCall_init()
     left_wheel= sim.getObjectHandle('joint2') 
     right_velocity=0 
     left_velocity=0
-    dVel=0.5; 
-    sim.getJointVelocity(right_wheel,1000) 
-    sim.getJointVelocity(left_wheel,1000) 
+    speed=8
+    sim.setJointTargetVelocity(right_wheel,0)
+    sim.setJointTargetVelocity(left_wheel,0)
+ 
 end
   
 function sysCall_actuation()  
     message,auxiliaryData=sim.getSimulatorMessage() 
     while message~=-1 do
         if (message==sim.message_keypress) then
+            
             if (auxiliaryData[1]==32) then
                 -- right key 
                 right_velocity=0 
@@ -25,26 +27,29 @@ function sysCall_actuation()
             
             
                 if (auxiliaryData[1]==2007) then -- up key
-                    right_velocity=right_velocity+1 
-                    left_velocity=left_velocity+1
+                    
+                    sim.setJointTargetVelocity(right_wheel,speed)
+                    sim.setJointTargetVelocity(left_wheel,speed)
                 end
                 if (auxiliaryData[1]==2008) then -- down key
-                    right_velocity=-1000
-                    left_velocity=-1000
+                    
+                    sim.setJointTargetVelocity(right_wheel,-speed/2)
+                    sim.setJointTargetVelocity(left_wheel,-speed/2)
+                    
                 end
                 if (auxiliaryData[1]==2009) then -- left key
-                    right_velocity=1000 
-                    left_velocity=0
+                    sim.setJointTargetVelocity(right_wheel,speed)
+                    sim.setJointTargetVelocity(left_wheel,speed/2)
+                   
                 end
                 if (auxiliaryData[1]==2010) then -- right key
-                    right_velocity=0
-                    left_velocity=1000 
+                    sim.setJointTargetVelocity(right_wheel,speed/2)
+                    sim.setJointTargetVelocity(left_wheel,speed)
+                    
                 end
+
             end
         end
         message,auxiliaryData=sim.getSimulatorMessage()
     end
-    sim.getJointTargetVelocity(right_wheel,right_velocity)
-    sim.getJointTargetVelocity(left_wheel,left_velocity)
-
 end
